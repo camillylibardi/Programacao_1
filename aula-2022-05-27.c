be6d770b0c28380cca214fd44771babcf34a2c4e
camillylibardi <106412161+camillylibardi@users.noreply.github.com>
2022-05-27 22:06:19 -0300

    Add files via upload

---
+++ aula-2022-05-27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+ printf(" Oi mundo\n");
+ printf("%.2d\n", 13);
+ printf ("%5.2f\n", 3.14);
+ printf("%5.2f\n", 3.14);
+ printf("%.2f\n", 27.2);
+ printf("%s\n", "-Lily? After all this time?\n-Always");
+ printf("%10d\n", 27);
+ printf("%010d\n", 27);
+ printf("Nome: %s\nIdade: %d\n",
+        "Camilly Eduarda", 19);
+} 
+
