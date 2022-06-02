720d1c1b81219f7f0bd022d2aa29aaf4d901908e
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-02 19:53:33 -0300

    Add files via upload

---
+++ Exercicios_de_sala/aula.c
@@ -0,0 +1,41 @@
+// Camilly Eduarda Fernandes de Paula Libardi
+// 2021100509
+/*
+inicio:
+	 
+*/
+
+
+#include<stdio.h>
+int main()
+{ float seg, segs, ter,ters, qua,quas, quin,quins, sex, sexs, pag, soma; 
+  printf ("Digite o valor recebido na segunda:\n");
+  scanf ("%f", &seg);
+  printf (" Digite as saidas de segunda:\n"); 
+  scanf ("%f", &segs);
+  printf ("Digite o valor recebido na terca:\n");
+  scanf ("%f", &ter);
+  printf (" Digite as saidas de terca:\n"); 
+  scanf ("%f", &ters);
+  printf ("Digite o valor recebido na quarta:\n");
+  scanf ("%f", &qua);
+  printf (" Digite as saidas de quarta:\n"); 
+  scanf ("%f", &quas);
+  printf ("Digite o valor recebido na quinta:\n");
+  scanf ("%f", &quin);
+  printf (" Digite as saidas de quinta:\n"); 
+  scanf ("%f", &quins);
+  printf ("Digite o valor recebido na sexta:\n");
+  scanf ("%f", &sex);
+  printf (" Digite as saidas de sexta:\n"); 
+  scanf ("%f", &sexs);
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+
