4640967331f9e08f3ca420d624d6092848c8417f
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-03 21:44:42 -0300

    Update aula-2022-06-02.c

--- Exercicios_de_sala/aula-2022-06-02.c
+++ Exercicios_de_sala/aula-2022-06-02.c
@@ -1,44 +1,110 @@
 #include <stdio.h>
 
 int main(void) {
-  float seg, segs, ter, ters, qua, quas, qui, quis, sex, sexs, despesas, recebidos, caixa;
-
+  float seg, segs, ter, ters, qua, quas, qui, quis, sex, sexs, despesas, recebidos, caixas, caixat, caixaq, caixaqu, caixase;
+  
+  
+  //Dados de segunda
   printf("Insira quanto ganhou na segunda-feira:\n");
-scanf("%f", &seg);
-printf("Insira quanto gastou na segunda-feira:\n");
-scanf("%f", &segs);
-printf("Insira quanto ganhou na terça-feira:\n");
-scanf("%f", &ter);
-printf("Insira quanto gastou na terça-feira:\n");
-scanf("%f", &ters);
-printf("Insira quanto ganhou na quarta-feira:\n");
-scanf("%f", &qua);
-printf("Insira quanto gastou na quarta-feira:\n");
-scanf("%f", &quas);
-printf("Insira quanto ganhou na quinta-feira:\n");
-scanf("%f", &qui);
-printf("Insira quanto gastou na quinta-feira:\n");
-scanf("%f", &quis);
-printf("Insira quanto ganhou na sexta-feira:\n");
-scanf("%f", &sex);
-printf("Insira quanto gastou na sexta-feira:\n");
-scanf("%f", &sexs);
-
-despesas=segs+ters+quas+quis+sexs;
-  printf("Você teve um total de saídas igual a:%f\n", despesas);
+  scanf("%f", &seg);
+  printf("Insira quanto gastou na segunda-feira:\n");
+  scanf("%f", &segs);
+  caixas= seg- segs;
   
-recebidos=seg+ter+qua+qui+sex;
-printf("Nessa semana a quantia que você recebeu é igual a:%f\n", recebidos);
+  if (caixas > 2000)
+  {
+  printf ("Valor recebido de segunda:%f\n",seg);
+  printf ("Valor de despesas da segunda:%f\n", segs);
+  printf (" O saldo positivo é:%f\n", caixas);
+  }
+  else if (caixas < -500) 
+  {
+  printf ("Valor recebido de segunda:%f\n",seg);
+  printf ("Valor de despesas da segunda:%f\n", segs);
+  printf (" O saldo negativo é:%f\n", caixas);
+  }
   
-  caixa=recebidos-despesas;
-  printf("Nesta semana seu saldo foi de:%f\n", caixa);
-
-if (caixa >= 2000)
-  printf("Seu saldo é positivo!!!");
-  else if (caixa<=2000 && caixa >=-500)
-    printf("Seu saldo está okay!!!");
-else (caixa<= -500);
-printf("Seu saldo é negativo");
+  //Dados de terça
+  printf("Insira quanto ganhou na terça-feira:\n");
+  scanf("%f", &ter);
+  printf("Insira quanto gastou na terça-feira:\n");
+  scanf("%f", &ters);
+  caixat= ter- ters;
+  
+  if (caixat > 2000)
+  {
+  printf ("Valor recebido de terça:%f\n",ter);
+  printf ("Valor de despesas da terça:%f\n", ters);
+  printf (" O saldo positivo é:%f\n", caixat);
+  }
+  else if (caixat < -500) 
+  {
+  printf ("Valor recebido de terça:%f\n",ter);
+  printf ("Valor de despesas da terça:%f\n", ters);
+  printf (" O saldo negativo é:%f\n", caixat);
+  }
+  
+  //Dados de quarta
+  printf("Insira quanto ganhou na quarta-feira:\n");
+  scanf("%f", &qua);
+  printf("Insira quanto gastou na quarta-feira:\n");
+  scanf("%f", &quas);
+  caixaq= qua-quas;
+  
+   if (caixaq > 2000)
+  {
+  printf ("Valor recebido de quarta:%f\n",qua);
+  printf ("Valor de despesas da quarta:%f\n", quas);
+  printf (" O saldo positivo é:%f\n", caixaq);
+  }
+  else if (caixaq < -500) 
+  {
+  printf ("Valor recebido de quarta:%f\n",qua);
+  printf ("Valor de despesas da quarta:%f\n", quas);
+  printf (" O saldo negativo é:%f\n", caixaq);
+  }
+  
+  //Dados de quinta
+  printf("Insira quanto ganhou na quinta-feira:\n");
+  scanf("%f", &qui);
+  printf("Insira quanto gastou na quinta-feira:\n");
+  scanf("%f", &quis);
+  caixaqu= qui-quis;
+  
+  if (caixaqu > 2000)
+  {
+  printf ("Valor recebido de quinta:%f\n",qui);
+  printf ("Valor de despesas da quinta:%f\n", quis);
+  printf (" O saldo positivo é:%f\n", caixaqu);
+  }
+  else if (caixaqu < -500) 
+  {
+  printf ("Valor recebido de quinta:%f\n",qui);
+  printf ("Valor de despesas da quinta:%f\n", quis);
+  printf (" O saldo negativo é:%f\n", caixaqu);
+  }
+  
+  //Dados de sexta
+  printf("Insira quanto ganhou na sexta-feira:\n");
+  scanf("%f", &sex);
+  printf("Insira quanto gastou na sexta-feira:\n");
+  scanf("%f", &sexs);
+  caixase= sex-sexs;
+  
+  if (caixase > 2000)
+  {
+  printf ("Valor recebido de sexta:%f\n",sex);
+  printf ("Valor de despesas da sexta:%f\n", sexs);
+  printf (" O saldo positivo é:%f\n", caixase);
+  }
+  else if (caixase < -500) 
+  {
+  printf ("Valor recebido de sexta:%f\n",sex);
+  printf ("Valor de despesas da sexta:%f\n", sexs);
+  printf (" O saldo negativo é:%f\n", caixase);
+  }
+  
+  
 
   return 0;
 }
