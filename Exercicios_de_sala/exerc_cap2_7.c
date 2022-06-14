0b87f35ac2e9e3eb544e8509385bcd3cbd014852
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-14 16:08:31 -0300

    Create exerc_cap2_7.c

---
+++ Exercicios_de_sala/exerc_cap2_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+  float num; 
+  printf ("Digite um número: \n");
+  scanf ( "%f", &num);
+  
+    
+  if ( num> 20 && num<50 ){
+	  printf (" O número está compreendido entre 20 e 50\n");
+  }
+  else
+	    printf ("O número não está compreendido entre 20 e 50");
+	
+
+	
+}
