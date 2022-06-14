7f41f07c7bdc5a6d5d7a1de5f73ac0ca440f6ea7
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-13 22:09:57 -0300

    Update exerc_cap2_5.c

--- Exercicios_de_sala/exerc_cap2_5.c
+++ Exercicios_de_sala/exerc_cap2_5.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 int main (){
-  float num, div3, div7;
+  int num, result3, rest3, rest7, result7;
   printf ("Digite um número: \n");
-  scanf ( "%f", &num);
+  scanf ( "%d", &num);
+  result3 = num/3;
+  result7 = num/7;
+  rest3 = num - result3*3;
+  rest7 = num - result7*7;  
   
-  div3= num/3;
-  div7= num/7;
-  
-  if (div3 == 0){
-	  printf ("Seu numero é divisivel por 3!");
-  }
-  else if (div7 == 0) {
-	    printf ("Seu numero é divisivel por 7!");
-  }
-  else if (div3 == 0 && div7 == 0){
+  if (rest3 == 0 && rest7 == 0 ){
 	  printf ("Seu numero é divisivel por 3 e 7!");
   }
+  else if (rest3 == 0){
+	  printf ("Seu numero é divisivel por 3!");
+  }
+  else if (rest7== 0) {
+	    printf ("Seu numero é divisivel por 7!");
+  }
+  
   else 
 	    printf ("Seu numero não é divisivel nem por 3, nem por 7!");
+	
+
+	
 }
+
