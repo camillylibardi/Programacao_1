6f0bbb1a7f7608f0ee53a8e4547387fe9c25634c
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-14 23:32:54 -0300

    Update exerc_cap2_13.switch.c

--- Exercicios_de_sala/exerc_cap2_13.switch.c
+++ Exercicios_de_sala/exerc_cap2_13.switch.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
-int main (){
-  char nome[50];
-  float p1, p2, media;
-  printf ("Digite seu nome:\n");
-  scanf ("%s", &nome);
-  printf ("Digite a note da P1:\n");
-  scanf ("%f", &p1);
-  printf ("Digite a note da P2:\n");
-  scanf ("%f", &p2);
-  media = (p1+p2)/2;
-  printf ("A media foi: %f\n", media);
-  
-  
+int main() {
+char nome [50]; 
+int nota1, nota2, media;
+printf ("Digite o seu nome: \n");
+scanf("%s", & nome);
+printf ("Digite a nota da prova 1: \n");
+scanf ("%d", & nota1);
+printf ("Digite a nota da prova 2: \n");
+scanf ("%d", & nota2);
+media = (nota1 + nota2)/2;
+
+switch (media) {
+
+  case 7 ... 10:
+  printf ("Seu nome é %s \n Você tirou %d na primeira prova \n Você tirou %d na segunda prova \n Sua média é %d \n Situação: Aprovado", nome, nota1, nota2, media);
+break;
+  case 0 ... 3: 
+  printf ("Seu nome é %s \n Você tirou %d na primeira prova \n Você tirou %d na segunda prova \n Sua média é %d \n Situação: Reprovado", nome, nota1, nota2, media);
+break;
+  default:
+  printf ("Seu nome é %s \n.Você tirou %d na primeira prova\n.Você tirou %d na segunda prova\n.Sua média é %d \n Situação: Prova final", nome, nota1, nota2, media);
 }
+
+
+
+  
+  
+  
+  
+  
+  
+  
+
+} 
