9934e9380f06632ef0eec9eec1ec582f0305938f
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-06-24 20:57:30 -0300

    Create exercícios-de-revisão-2.c

---
+++ Exercicios_de_sala/exercícios-de-revisão-2.c
@@ -0,0 +1,23 @@
+/* Variaveis:
+    int N,M, vez=0;
+   Inicio
+    Ler valor de N e M
+    EScrever total N na tela
+    Enquanto meu valor de N>0:
+        Se vez==0:
+           EScreve 'Paula  -'
+           vez 1
+        Senao
+           Escrev 'Carlos -'
+           vez 0
+        N-=M 
+        Se N<0:
+          Imprime 0
+        Senao 
+          Imprime N
+    Se vez==0: 
+      Imprime Carlos ganhou
+    Senão 
+      Imprime Paula gahou.
+*/
+
