da7d7b9a18213462fcb9b88fe27b63ceb6865621
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-07-08 21:49:16 -0300

    Update 2022-07-08-vetores4.c

--- Exercicios_de_sala/2022-07-08-vetores4.c
+++ Exercicios_de_sala/2022-07-08-vetores4.c
@@ -1,14 +1,15 @@
 /*Crie um vetor, de forma automática, com a seguinte P.A.:
 2n+1, onde n = índice do vetor.
-Ele deve ser do tipo float e ter 100 posições.*/
+Ele deve ser do tipo float e ter 100 posições.
+Após isso, imprima os 10 primeiros valores do vetor na tela.*/
 
 #include <stdio.h>
 int main(){
-    int vet[100],i;
+    float vet[100],i;
     for (i=0; i<100; i++){
        vet [i]= (2*i)+1;
     }
-    for (i=0; i<100; i++){
+    for (i=0; i<10; i++){
        printf ("%d\n", vet[i]);
     }
 }
