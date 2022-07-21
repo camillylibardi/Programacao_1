33923a0cf000c5548f26702a01eb0c6906b9d882
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-07-21 19:17:57 -0300

    Update para-casa-2022-07-15.c

--- Exercicios_de_sala/para-casa-2022-07-15.c
+++ Exercicios_de_sala/para-casa-2022-07-15.c
@@ -1,24 +1,28 @@
 //escreva todos os números dessa matriz na tela
+
 #include <stdio.h>
-int main ()
-{
-int matriz [10][10]; 
-int i, j;
-srand(time (NULL));
-for (i=0; i<10; i++){
-  for (j=0;j<10; j++){
-    matriz[i][j] = rand()%100;
+#include <stdlib.h>
+
+int main() {
+    int mat[10][10], lin, col, num_impar[10]={0,0,0,0,0,0,0,0,0,0}, i;
+    
+    for (lin=0; lin<10; lin++){
+        for (col=0; col<10; col++){
+            mat[lin][col]= rand()%1000;
+        }
     }
-  }
-for (i=0; i<10; i++){
-  for (j=0;j<10; j++){
-    printf ("%d", matriz[i][j]);
-    printf ("   ");
+    for (lin=0; lin<10; lin++){
+        for (col=0; col<10; col++){
+            if (mat[lin][col]%2==1)
+                num_impar[col]++;
+        }
     }
-    printf("\n");
-  }
- 
+    for (i=0; i<10; i++){
+        printf ("coluna %d:",i);
+        printf ("%d\n", num_impar[i]);
+    }
 }
+
 
 //informe quantos números ímpares tem em cada coluna
 
