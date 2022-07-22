990c53c48ec4a49cb519e3c1c07b95cc94e93d6b
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-07-22 20:54:18 -0300

    Update 2022-07-22-struct.c

--- Exercicios_de_sala/2022-07-22-struct.c
+++ Exercicios_de_sala/2022-07-22-struct.c
@@ -25,5 +25,48 @@ int main (){
     scanf ("%s", var1.nome);
     printf ("Digite sua idade:\n");
     scanf ("%d", &var1.idade);
+    printf ("Seu nome é:%s.\nSua idade é:%d\n",var1.nome, var1.idade);
+    
+}
+
+
+// outra com typedef
+
+#include <stdio.h>
+
+typedef struct{
+        char nome[20];
+        int idade;
+}pessoa;
+
+int main (){
+    pessoa var1;
+    printf ("Digite seu nome:\n");
+    scanf ("%s", var1.nome);
+    printf ("Digite sua idade:\n");
+    scanf ("%d", &var1.idade);
+    printf ("Seu nome é:%s.\nSua idade é:%d anos\n",var1.nome, var1.idade);
+    
+}
+
+// outra aluno sem vetor
+#include <stdio.h>
+
+typedef struct{
+        char nome[20];
+        int idade;
+        float nota;
+}tipo_aluno;
+
+int main (){
+    tipo_aluno aluno;
+    printf ("Digite seu nome:\n");
+    scanf ("%s", aluno.nome);
+    printf ("Digite sua idade:\n");
+    scanf ("%d", &aluno.idade);
+    printf ("Digite sua nota:\n");
+    scanf ("%f", &aluno.nota);
+    
+    printf ("Seu nome é:%s.\nSua idade é:%d anos\nSua nota:%.2f",aluno.nome, aluno.idade, aluno.nota);
     
 }
