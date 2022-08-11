01ad37b100196cee5c27df32a1cfb136efce488c
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-08-11 19:51:49 -0300

    Update 2022-08-11-funcaodentrodefuncao.c

--- Exercicios_de_sala/2022-08-11-funcaodentrodefuncao.c
+++ Exercicios_de_sala/2022-08-11-funcaodentrodefuncao.c
@@ -29,4 +29,44 @@ int main (){
     esc_num (10);
 }
 
-// 
+// par ou impar
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int p_impar (int vet[5], int pos){
+   if (0<= pos && pos >5){
+        if ( (vet[pos]%2) == 1){
+            return 1;}
+        else {
+            return 0;}
+    }else {
+        return 0;}
+     
+}
+int main (){
+    int v[5], resp;
+    resp= p_impar (v, 0);
+    printf ("Resp: %s\n", (resp==1)? "sim":"nao");
+}
+
+// par ou impar com a função
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int p_impar (int vet[5], int pos){
+   if (0<= pos && pos <5){
+        if ( (vet[pos]%2) == 1){
+            return 1;}
+        else {
+            return p_impar (vet, pos+1);}
+    }else {
+        return 0;}
+     
+}
+int main (){
+    int v[5]= {2, 4, 6, 1, 7}, resp;
+    resp= p_impar (v, 0);
+    printf ("Resp: %s\n", (resp==1)? "sim":"nao");
+}
