ab5d488c0fad502d05299ccec1a3a98da5565a0a
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-08-11 19:05:28 -0300

    Update 2022-08-11-funcaodentrodefuncao.c

--- Exercicios_de_sala/2022-08-11-funcaodentrodefuncao.c
+++ Exercicios_de_sala/2022-08-11-funcaodentrodefuncao.c
@@ -15,4 +15,18 @@ float csoma (float vet[10]){
       return s;     
 }
 
-//outra
+//outra para colocar em ordem decrescente 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void esc_num (int num){
+    printf ("Número:%d\n", num);
+    if (num>0)
+        esc_num (num-1);
+}
+int main (){
+    esc_num (10);
+}
+
+// 
