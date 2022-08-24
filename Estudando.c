0a92435de024a67a963103d7eaf312fd52ba2eae
Camilly Libardi <106412161+camillylibardi@users.noreply.github.com>
2022-08-23 22:00:10 -0300

    Update Estudando.c

--- Estudando.c
+++ Estudando.c
@@ -49,3 +49,88 @@
  printf("\nDivisao: %f", div);
  }
 
+// if e else
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+int n;
+printf ("Digite um número:\n");
+scanf ("%d", &n);
+if (n<20){
+  printf ("Resultado:%d\n", n*4);
+}
+else if (n==20)
+  printf ("Resultado:%d\n",n);
+else 
+printf ("Resultado:%d\n", n*2);
+}
+
+// Voluntaroi apto
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+char nome [50];
+int idade, resfriado, bemalimentado;
+float peso;
+
+printf ("Digite seu nome:\n");
+scanf ("%s", &nome);
+printf ("Sua idade:\n");
+scanf ("%d", &idade);
+printf ("Digite seu peso:\n");
+scanf ("%f", &peso);
+printf ("Você esta resfriado? 1- sim, 0-não:\n");
+scanf ("%d", &resfriado);
+printf ("Você está bem alimentado? 1-sim, 0-não\n");
+scanf ("%d", &bemalimentado);
+
+if (idade >= 16 && idade <=69 && peso >= 50 && resfriado==0 && bemalimentado==1 ){
+printf ("O voluntário %s está apto", nome);
+}
+else
+printf ("O voluntário %s não esta apto", nome);
+  }
+
+// Peso nos planetas
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+int n;
+float pesoterra, pesoplaneta;
+
+printf ("Digite seu peso na Terra\n");
+scanf ("%f", &pesoterra);
+printf ("1-Mercúrio, 2-Venus, 3-Marte,\n 4-Júpiter, 5-Saturno, 6-Urano\n");
+printf ("Digite o numero do planeta:\n");
+scanf ("%d", &n);
+switch (n){
+  case 1:
+  pesoplaneta= (pesoterra/10)*0.37;
+  break;
+  case 2:
+  pesoplaneta= (pesoterra/10)*0.88;
+  break;
+  case 3:
+  pesoplaneta= (pesoterra/10)*0.38;
+  break;
+  case 4:
+  pesoplaneta= (pesoterra/10)*2.64;
+  break;
+  case 5:
+  pesoplaneta= (pesoterra/10)*1.15;
+  break;
+  case 6:
+  pesoplaneta= (pesoterra/10)*1.17;
+  break;
+  default:
+  printf("Peso inválido!\n");
+  
+}
+printf ("Peso no planeta:%f", pesoplaneta);
+  }
